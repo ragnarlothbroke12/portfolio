db224f2209474423752aa0a02124fd80ea4188b9
ragnarlothbroke12 <siyaram490don@gmail.com>
2025-10-22 12:20:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+ let menu = document.querySelector('#menu-icon');
+ let navbar = document.querySelector('.navbar');
+
+ menu.onclick = () => {
+     menu.classList.toggle('bx-x');
+     navbar.classList.toggle('active');
+ }
+
+ window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+ }
+
+ const typed = new Typed('.multiple-text', {
+      strings: ['Frontend Developer','Backend Developer' ,'Blockchain Developer','Web Designer'],
+      typeSpeed: 80,
+      backSpeed: 80,
+      backDelay: 1200,
+      loop: true,
+    });
+
+    // Skill bar animation on scroll
+window.addEventListener("scroll", () => {
+  const bars = document.querySelectorAll(".line");
+  bars.forEach((bar) => {
+    const barPosition = bar.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight - 100;
+
+    if (barPosition < screenPosition) {
+      bar.classList.add("active");
+    }
+  });
+});
